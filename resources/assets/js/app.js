c6ea4fb72bfac7777f19dd2c3ea637c2dd6e2e15
Ilyes512 <ilyes.ahidar@gmail.com>
2017-01-03 23:30:00 +0100

    Added initial needed node packages

--- resources/assets/js/app.js
+++ resources/assets/js/app.js
@@ -1,11 +1,5 @@
-
-/**
- * First we will load all of this project's JavaScript dependencies which
- * include Vue and Vue Resource. This gives a great starting point for
- * building robust, powerful web applications using Vue and Laravel.
- */
-
 require('./bootstrap');
+var Vue = require('vue');
 
 /**
  * Next, we will create a fresh Vue application instance and attach it to
@@ -18,3 +12,4 @@ Vue.component('example', require('./components/Example.vue'));
 const app = new Vue({
     el: '#app'
 });
+
